e983cbc75743c21ce8758cd2c9ffead8d349c783
250541027_e.zehra <250541027@firat.edu.tr>
2025-11-06 19:43:41 +0300

    Update OgrenciBilgi.java

--- OgrenciBilgi.java
+++ OgrenciBilgi.java
@@ -6,55 +6,40 @@
  * 
  * Bu program kullanicidan ogrenci bilgilerini alir ve
  * duzenli bir formatta ekrana yazdirir.
- * Diğer java dosyalarının başında da bu örnek formattaki gibi kısa bilgi giriniz
- */
+*/
 
-import java.util.Scanner;
-
+import java.util.Scanner ;
 public class OgrenciBilgi {
-    public static void main(String[] args) {
-        // Scanner objesi olusturun
-        
-        
-        // Degisken tanimlamalari
-        // String ad, soyad;
-        // int ogrenciNo, yas;
-        // double gpa;
-        
-        
-        // Kullanicidan bilgileri alin
+    public static void main(String[]args){
+        Scanner input= new Scanner(System.in);
         System.out.println("=== OGRENCI BILGI SISTEMI ===");
-        System.out.println();
         
-        // Ad
-        
-        
-        // Soyad
-        
-        
-        // Ogrenci No
-        
-        
-        // Yas
-        
-        
-        // GPA
-        
-        
-        // Bilgileri ekrana yazdirin
-        System.out.println("\n=== OGRENCI BILGI SISTEMI ===");
-        
-        // COZUMUNUZU BURAYA YAZIN
-        
-        
-        
-        
-        
-        
+        // KULLANICIDAN ALINACAK BİLGİLER 
+        // AD-SOYAD
+        System.out.print("Adinizi Giriniz=");
+        String ad = input.nextLine();
+        System.out.print("Soyadinizi Giriniz=");
+        String soyad = input.nextLine();
+        // OGRENCİ NUMARASI
+        System.out.print("Ogrenci Numaraniz=");
+        int ogrenciNo = input.nextInt();
+        // YAS
+        System.out.print("Yasinizi girin=");
+        int yas = input.nextInt();
+        //GPA
+        System.out.print("GPA (0.00-4.00): ");
+        double gpa = input.nextDouble();
+        // DURUM
+        String durum= (gpa >=2) ? "Basarili Ogrenci" : "Basarisiz Ogrenci";
 
-        
-        
-        // Scanner'i kapatin (önemli pratik)
-        
+        // ÇIKTILAR 
+        System.out.println("=== Ogrenci Bilgi Sistemi===");
+        System.out.printf("Ad Soyad : %s %s\n",ad,soyad);
+        System.out.printf("Ogrenci No: %d\n", ogrenciNo);
+        System.out.printf("Yas: %d\n", yas );
+        System.out.printf(" GPA: %.2f\n", gpa);
+        System.out.printf("Durum: %s\n",durum);
+        input.close();
     }
+
 }
